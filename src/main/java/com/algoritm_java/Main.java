03f0b5871477f76dd56bcdf82bf292122f90e059
BanseokSuh <still3028@gmail.com>
2023-04-27 07:35:53 +0900

    first commit

---
+++ src/main/java/com/algoritm_java/Main.java
@@ -0,0 +1,8 @@
+package com.algoritm_java;
+
+public class Main {
+    private static Solution001 s = new Solution001();
+    public static void main(String[] args) {
+//        s.solution();
+    }
+}
---
+++ src/main/java/com/algoritm_java/Solution001.java
@@ -0,0 +1,8 @@
+package com.algoritm_java;
+
+class Solution001 {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = {};
+        return answer;
+    }
+}
