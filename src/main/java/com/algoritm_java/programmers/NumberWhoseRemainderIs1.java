d657e1f2453fce9ab3485b05cb96920f79717c68
BanseokSuh <still3028@gmail.com>
2023-10-31 21:40:08 +0900

    Programmers_lv1_NumberWhoseRemainderIs1

---
+++ src/main/java/com/algoritm_java/programmers/NumberWhoseRemainderIs1.java
@@ -0,0 +1,22 @@
+package com.algoritm_java.programmers;
+
+public class NumberWhoseRemainderIs1 {
+    public static int solution(int n) {
+        int result = 0;
+
+        for (int x = 1; x <= n; x++) {
+            if (n % x == 1) {
+                result = x;
+                break;
+            }
+        }
+        return result;
+    }
+
+
+    public static void main(String[] arg) {
+        int test1 = 12;
+        int result1 = solution(test1);
+        System.out.println("result1 = " + result1);
+    }
+}
