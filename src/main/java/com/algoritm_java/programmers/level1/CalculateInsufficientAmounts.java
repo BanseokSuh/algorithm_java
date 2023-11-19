f2b0bf609f9969a75319afa1cb34528a7e788bc0
BanseokSuh <still3028@gmail.com>
2023-11-19 20:58:52 +0900

    Programmers_lv1_CalculateInsufficientAmounts

---
+++ src/main/java/com/algoritm_java/programmers/level1/CalculateInsufficientAmounts.java
@@ -0,0 +1,25 @@
+package com.algoritm_java.programmers.level1;
+
+public class CalculateInsufficientAmounts {
+    public static long solution(int price, int money, int count) {
+        long totalAmount = 0;
+
+        for (int i = 1; i <= count; i++) {
+            totalAmount += price * i;
+        }
+
+        long required = totalAmount - money;
+
+        return Math.max(required, 0);
+    }
+
+    public static void main(String[] args) {
+        int price1 = 3;
+        int money1 = 20;
+        int count1 = 4;
+
+        long answer1 = solution(price1, money1, count1);
+
+        System.out.println("answer1 = " + answer1);
+    }
+}
