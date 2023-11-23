9980926c85d1c51b564525ad6bed63b0285106ce
BanseokSuh <still3028@gmail.com>
2023-11-24 01:49:52 +0900

    Programmers_lv1_PlayersWhoDidNotComplete

---
+++ src/main/java/com/algoritm_java/programmers/category/hash/PlayersWhoDidNotComplete.java
@@ -0,0 +1,39 @@
+package com.algoritm_java.programmers.category.hash;
+
+import java.util.Arrays;
+
+public class PlayersWhoDidNotComplete {
+    public static String solution(String[] participant, String[] completion) {
+        /**
+         * 두 배열 sort해서 다르면 리턴
+         * 중간에 다른 요소 없으면 participant의 제일 마지막 요소 리턴
+         */
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        for (int i = 0; i < completion.length; i++) {
+            if (!participant[i].equals(completion[i])) return participant[i];
+        }
+
+        return participant[participant.length-1];
+    }
+
+    public static void main(String[] args) {
+        String[] participant1 = {"leo", "kiki", "eden"};
+        String[] completion1 = {"eden", "kiki"};
+
+        String[] participant2 = {"marina", "josipa", "nikola", "vinko", "filipa"};
+        String[] completion2 = {"josipa", "filipa", "marina", "nikola"};
+
+        String[] participant3 = {"mislav", "stanko", "mislav", "ana"};
+        String[] completion3 = {"stanko", "ana", "mislav"};
+
+        String answer1 = solution(participant1, completion1);
+        String answer2 = solution(participant2, completion2);
+        String answer3 = solution(participant3, completion3);
+
+        System.out.println("answer1 = " + answer1);
+        System.out.println("answer2 = " + answer2);
+        System.out.println("answer3 = " + answer3);
+    }
+}
