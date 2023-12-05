7613c01a9f9acff9d6b16340a000a527e1205f8e
BanseokSuh <still3028@gmail.com>
2023-12-05 21:19:18 +0900

    Programmers_Stackandqueue_Lv2_BridgeTruck (약간의 설명)

--- src/main/java/com/algoritm_java/programmers/category/stackandqueue/Lv2_BridgeTruck.java
+++ src/main/java/com/algoritm_java/programmers/category/stackandqueue/Lv2_BridgeTruck.java
@@ -1,31 +1,49 @@
 package com.algoritm_java.programmers.category.stackandqueue;
 
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class Lv2_BridgeTruck {
     public static int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
         int totalWeight = 0;
 
-        LinkedList<Integer> bridge = new LinkedList<>();
+        Queue<Integer> bridge = new LinkedList<>();
 
+        /**
+         * bridge queue 초기화
+         */
         for (int i = 0; i < bridge_length; i++) {
             bridge.add(0);
         }
 
+        /**
+         * 대기 트럭만큼 loop
+         */
         for (int truck : truck_weights) {
+            /**
+             * 대상이 되는 트럭과 다리의 가장 마지막 요소의 무게가 weight를 넘으면
+             *  - 왼쪽으로 한 칸씩 이동
+             *  - 오른쪽에 0으로 채움
+             */
             while ((totalWeight + truck - bridge.peek()) > weight) {
                 totalWeight -= bridge.poll();
                 bridge.add(0);
                 answer++;
             }
 
+            /**
+             * 다리에 트럭 담고 ++
+             */
             totalWeight -= bridge.poll();
             totalWeight += truck;
             bridge.add(truck);
             answer++;
         }
 
+        /**
+         * loop가 끝나는 시점은 다리 제일 끝에 트럭이 있기 때문에, 트럭이 다리를 다 지나올 때까지를 의미하는 bridge_length를 더해서 반환
+         */
         return answer + bridge_length;
     }
 
