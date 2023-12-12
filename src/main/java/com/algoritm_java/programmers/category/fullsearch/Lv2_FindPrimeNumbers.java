3d9c31234a33c3dc75a7654d20edeba1698b7f70
BanseokSuh <still3028@gmail.com>
2023-12-12 21:42:45 +0900

    Programmers_FullSearch_Lv2_FindPrimeNumbers_v2

--- src/main/java/com/algoritm_java/programmers/category/fullsearch/Lv2_FindPrimeNumbers.java
+++ src/main/java/com/algoritm_java/programmers/category/fullsearch/Lv2_FindPrimeNumbers.java
@@ -1,35 +1,28 @@
 package com.algoritm_java.programmers.category.fullsearch;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Iterator;
 
+/**
+ * [1] 재귀
+ * [2] Iterator
+ * [3] 에라토스테네스의 체
+ */
 public class Lv2_FindPrimeNumbers {
-    static List<Integer> list = new ArrayList<>();
-    static boolean[] visited = new boolean[7];
-
     /**
-     * dfs
-     * @param numbers
-     * @param cur
-     * @param m
+     * hashSet
      */
-    public static void dfs(String numbers, String cur, int m) {
-        if (cur.length() == m) {
-            int num = Integer.parseInt(cur);
+    HashSet<Integer> numberSet = new HashSet<>();
 
-            if (!list.contains(num)) {
-                list.add(num);
-            }
+    void recursive(String comb, String others) {
+        // 현재 조합을 set에 추가
+        if (!comb.equals("")) {
+            numberSet.add(Integer.valueOf(comb));
         }
 
-        for (int i = 0; i < numbers.length(); i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                cur += numbers.charAt(i);
-                dfs(numbers, cur, m);
-                visited[i] = false;
-                cur = cur.substring(0, cur.length() - 1);
-            }
+        // 남은 숫자 중 한 개를 더해서 새로운 조합 생성
+        for (int i = 0; i < others.length(); i++) {
+            recursive(comb + others.charAt(i), others.substring(0, i) + others.substring(i + 1));
         }
     }
 
@@ -38,12 +31,14 @@ public class Lv2_FindPrimeNumbers {
      * @param num
      * @return
      */
-    public static boolean isPrime(int num) {
+    public boolean isPrime(int num) {
         if (num < 2) {
             return false;
         }
 
-        for (int i = 2; i * i <= num; i++) {
+        int limit = (int) Math.sqrt(num);
+
+        for (int i = 2; i <= limit; i++) {
             if (num % i == 0) {
                 return false;
             }
@@ -52,21 +47,22 @@ public class Lv2_FindPrimeNumbers {
         return true;
     }
 
-    public static int solution(String numbers) {
+    public int solution(String numbers) {
         int answer = 0;
 
-        /**
-         * dfs로 순열 구하기
-         */
-        for (int i = 0; i < numbers.length(); i++) {
-            dfs(numbers, "", i+1);
-        }
+        // 재귀
+        recursive("", numbers);
 
-        /**
-         * 소수인지 판별하기
-         */
-        for (int i = 0; i < list.size(); i++) {
-            if (isPrime(list.get(i))) {
+        System.out.println("numberSet = " + numberSet);
+
+        // Iterator
+        Iterator<Integer> it = numberSet.iterator();
+
+        while (it.hasNext()) {
+            int number = it.next();
+
+            // 소수인지 판별 (에라토스테네스의 체)
+            if (isPrime(number)) {
                 answer++;
             }
         }
@@ -74,15 +70,16 @@ public class Lv2_FindPrimeNumbers {
         return answer;
     }
 
-
     public static void main(String[] args) {
-        String numbers1 = "17";
+        Lv2_FindPrimeNumbers instance = new Lv2_FindPrimeNumbers();
+
+//        String numbers1 = "17";
         String numbers2 = "011";
 
-        int answer1 = solution(numbers1);
-        int answer2 = solution(numbers2);
+//        int answer1 = instance.solution(numbers1);
+        int answer2 = instance.solution(numbers2);
 
-        System.out.println("answer1 = " + answer1);
+//        System.out.println("answer1 = " + answer1);
         System.out.println("answer2 = " + answer2);
     }
 }
