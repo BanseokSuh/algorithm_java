a2aa103e6163294b2b7e67fb5090cedd6f4847d9
BanseokSuh <still3028@gmail.com>
2023-12-16 21:41:23 +0900

    Programmers_Lv1_GetReportResults

--- src/main/java/com/algoritm_java/programmers/category/fullsearch/Lv2_Fatigue.java
+++ src/main/java/com/algoritm_java/programmers/category/fullsearch/Lv2_Fatigue.java
@@ -6,21 +6,21 @@ public class Lv2_Fatigue {
 
     public static void recursive(int depth, int k, int[][] dungeons) {
         for (int i = 0; i < dungeons.length; i++) {
-            int min = dungeons[i][0];
-            int use = dungeons[i][1];
+            int min = dungeons[i][0]; // 최소 피로도
+            int use = dungeons[i][1]; // 소모 피로도
 
-            if (!visited[i] && k >= min) {
-                int cur = k - use;
-                visited[i] = true;
-                recursive(depth+1, cur, dungeons);
-                visited[i] = false;
+            if (!visited[i] && k >= min) { // 방문하지 않았고 현재 피로도가 최소 피로도 이상일 경우
+                int cur = k - use; // 현재 피로도 갱신
+                visited[i] = true; // 방문 처리
+                recursive(depth+1, cur, dungeons); // 재귀
+                visited[i] = false; // 방문 해제 처리
             }
         }
         answer = Math.max(answer, depth);
     }
 
     public static int solution(int k, int[][] dungeons) {
-        visited = new boolean[dungeons.length];
+        visited = new boolean[dungeons.length]; // 방문한 던전 배열
 
         recursive(0, k, dungeons);
 
---
+++ src/main/java/com/algoritm_java/programmers/level/lv1/GetReportResults.java
@@ -0,0 +1,53 @@
+package com.algoritm_java.programmers.level.lv1;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class GetReportResults {
+    public static int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = new int[id_list.length];
+        Map<String, HashSet<String>> map = new HashMap<>();
+        Map<String, Integer> idxMap = new HashMap<>();
+
+        for (int i = 0; i < id_list.length; i++) {
+            String name = id_list[i];
+            map.put(name, new HashSet<>());
+            idxMap.put(name, i);
+        }
+
+        for (String s : report) {
+            String[] str = s.split(" ");
+            String from = str[0];
+            String to = str[1];
+            map.get(to).add(from);
+        }
+
+        for (int i = 0; i < id_list.length; i++) {
+            HashSet<String> send = map.get(id_list[i]);
+            if (send.size() >= k) {
+                for (String name : send) {
+                    answer[idxMap.get(name)]++;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String[] id_list1 = {"muzi", "frodo", "apeach", "neo"};
+        String[] report1 = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
+        int k1 = 2;
+
+        String[] id_list2 = {"con", "ryan"};
+        String[] report2 = {"ryan con", "ryan con", "ryan con", "ryan con"};
+        int k2 = 3;
+
+        int[] answer1 = solution(id_list1, report1, k1);
+        int[] answer2 = solution(id_list2, report2, k2);
+
+        System.out.println("answer1 = " + Arrays.toString(answer1));
+        System.out.println("answer2 = " + Arrays.toString(answer2));
+    }
+}
